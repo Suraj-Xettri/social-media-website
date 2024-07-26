4020f0fb49ccff21fb2445e417ccf8a8cc3dc640
Suraj Thapa <surajthapart@gmail.com>
2024-07-26 12:19:59 +0545

    little bit of change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Login } from "./forms/Login"
 import { Register } from "./forms/Register"
+import Nav from "./navbar/Nav"
 function App() {
   return (
-   <>
+   <div className="relative">
+    <Nav/>
     <Login/>
 
-   </>
+   </div>
   )
 }
 
---
+++ src/navbar/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-screen h-16 flex items-center px-6 absolute bg-black'>
+        <div className='text-white text-4xl'>Logo</div>
+    </div>
+  )
+}
+
+export default Nav
