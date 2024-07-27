a454994d83676585cec4d39f4e3190ad3fed6bff
Suraj Thapa <surajthapart@gmail.com>
2024-07-27 07:11:05 +0545

    Nav bar made

--- src/navbar/Nav.jsx
+++ src/navbar/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { TfiMenu } from "react-icons/tfi";
 
 const Nav = () => {
   return (
-    <div className='w-screen h-16 flex items-center px-6 absolute bg-black'>
-        <div className='text-white text-4xl'>Logo</div>
+    <div className='w-screen h-16 flex bg-black justify-between items-center px-10 absolute backdrop:blur-xl'>
+        <div className='text-white text-4xl flex gap-6 justify-center items-center'>
+          <p>AnimePlex</p>
+          <input type="text" placeholder='Search here..' className='px-4 py-2 text-sm w-[20vw] border-zinc-400 focus:outline-none focus:border-[3px] focus:border-blue-200' />
+        </div>
+
+
+        <div className='text-white flex gap-8 items-center'>
+          <button className='bg-red-600 px-4 py-2 rounded-xl font-bold '>Log in</button>
+          <TfiMenu className='text-4xl cursor-pointer'/>
+        </div>
     </div>
   )
 }
