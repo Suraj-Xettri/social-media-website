3de81b66a655a9468533867565218aa76ba7df51
Suraj Thapa <surajthapart@gmail.com>
2024-07-28 13:06:01 +0545

    MAde Nav responsive

--- src/navbar/Nav.jsx
+++ src/navbar/Nav.jsx
@@ -4,24 +4,24 @@ import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Nav = () => {
   return (
-    <div className="w-screen h-16 flex bg-black justify-between items-center px-10 absolute backdrop:blur-xl">
-      <div className="text-white text-4xl flex gap-6 justify-center items-center">
+    <div className="w-screen h-16 flex bg-black justify-between items-center sm:px-4 lg:px-10 absolute backdrop:blur-xl">
+      <div className="text-white text-2xl p-2 lg:text-4xl flex gap-6 justify-center items-center">
         <p> <Link to={'/'}>AnimePlex</Link> </p>
         <div className="flex items-center relative">
           <input
             type="text"
             placeholder="Search here.."
-            className="px-4 py-[10px] text-sm w-[20vw] border-zinc-400 focus:outline-none focus:border-[3px] focus:border-blue-200"
+            className="px-4 hidden sm:flex md:py-[10px] text-sm w-[20vw] border-zinc-400 focus:outline-none focus:border-[3px] focus:border-blue-200"
           />
-          <BsSearch className="absolute text-red-500  text-2xl right-1 cursor-pointer"/>
+          <BsSearch className="absolute hidden sm:flex text-red-500  text-2xl right-1 cursor-pointer"/>
         </div>
       </div>
 
-      <div className="text-white flex gap-8 items-center">
-        <button className="bg-red-600 px-4 py-2 rounded-xl font-bold hover:bg-red-500 transition duration-300 ease-out">
+      <div className="text-white flex pr-2 gap-3 lg:gap-8 items-center">
+        <button className="bg-red-600 px-4 py-[5px] md:px-4 md:py-2 rounded-xl font-bold hover:bg-red-500 transition duration-300 ease-out">
           <Link to={'/login'}>login</Link>
         </button>
-        <TfiMenu className="text-4xl cursor-pointer" />
+        <TfiMenu className="text-3xl md:text-4xl cursor-pointer" />
       </div>
     </div>
   );
