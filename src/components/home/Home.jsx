43fe4aba63425b38507962ef46af050fbaac159a
Suraj Thapa <surajthapart@gmail.com>
2024-07-29 12:42:18 +0545

    Home header part done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='pt-[64px]'>
-      
+    <div className="">
+      <div className="w-full h-[45vh] bg-[url('/public/animegroup.jpg')] bg-cover bg-no-repeat bg-center flex justify-center items-end">
+        <div className="font-bold text-2xl sm:text-4xl md:text-4xl lg:text-5xl text-zinc-200 w-screen text-center backdrop-blur bg-black bg-opacity-45 p-4 rounded-md">
+          <h1>
+            Discover the latest anime news
+          </h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/navbar/Nav.jsx
+++ src/navbar/Nav.jsx
@@ -4,14 +4,14 @@ import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Nav = () => {
   return (
-    <div className="w-screen h-16 flex bg-black justify-between items-center sm:px-4 lg:px-10 absolute backdrop:blur-xl">
+    <div className="w-screen h-16 flex justify-between items-center sm:px-4 lg:px-10 absolute backdrop-blur bg-black bg-opacity-50">
       <div className="text-white text-2xl p-2 lg:text-4xl flex gap-6 justify-center items-center">
         <p> <Link to={'/'}>AnimePlex</Link> </p>
         <div className="flex items-center relative">
           <input
             type="text"
             placeholder="Search here.."
-            className="px-4 hidden sm:flex md:py-[10px] text-sm w-[20vw] border-zinc-400 focus:outline-none focus:border-[3px] focus:border-blue-200"
+            className="px-4 py-2 hidden sm:flex md:py-[10px] text-sm w-[20vw] border-zinc-400 focus:outline-none focus:border-[3px] focus:border-blue-200"
           />
           <BsSearch className="absolute hidden sm:flex text-red-500  text-2xl right-1 cursor-pointer"/>
         </div>
