9809dec174cca3b24c00aeed920d65e87f69c46a
Suraj Thapa <surajthapart@gmail.com>
2024-08-12 15:10:08 +0545

    Not big change

--- back-end/controller/authController.js
+++ back-end/controller/authController.js
@@ -3,54 +3,53 @@ import bcrypt from "bcrypt";
 import generateToken from "../utils/generateToken.js";
 
 const registerUser = async (req, res) => {
-    try {
-      const { username, email, password } = req.body;
-  
-      let existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).send("User already exists with this email.");
-      }
-  
-      bcrypt.hash(password, 10, async (err, hash) => {
-        if (err) return res.send("error in hash", err.message);
-  
-        const user = await User.create({
-          username,
-          email,
-          password: hash,
-        });
-        let token = generateToken(user);
-        res.cookie("token", token);
-        res.send("Succesfully registered")
-      });
-    } catch (error) {
-      console.error("Error occurred during registration:", error.message);
-      res.status(500).send("Server error");
+  try {
+    const { username, email, password } = req.body;
+
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send("User already exists with this email.");
     }
-  }
 
-  const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      let existingUser = await User.findOne({ email });
-      if (!existingUser) return res.send("incorrect Username or password");
-      bcrypt.compare(password, existingUser.password, (err, result) => {
-        if (!result) return res.send("incorrect Username or password");
-  
-        let token = generateToken(existingUser);
-        res.cookie("token", token);
-        res.send("Success fully logged in");
+    bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) return res.send("error in hash", err.message);
+
+      const user = await User.create({
+        username,
+        email,
+        password: hash,
       });
-    } catch (error) {
-      console.error("Error occurred during registration:", error.message);
-      res.status(500).send("Server error");
-    }
+      let token = generateToken(user);
+      res.cookie("token", token);
+      res.send("Succesfully registered");
+    });
+  } catch (error) {
+    console.error("Error occurred during registration:", error.message);
+    res.status(500).send("Server error");
   }
+};
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let existingUser = await User.findOne({ email });
+    if (!existingUser) return res.send("incorrect Username or password");
+    bcrypt.compare(password, existingUser.password, (err, result) => {
+      if (!result) return res.send("incorrect Username or password");
 
-  const auth = {
-    registerUser,
-    login
+      let token = generateToken(existingUser);
+      res.cookie("token", token);
+      res.send("Success fully logged in");
+    });
+  } catch (error) {
+    console.error("Error occurred during registration:", error.message);
+    res.status(500).send("Server error");
   }
+};
 
-  export default auth
+const auth = {
+  registerUser,
+  login,
+};
+
+export default auth;
--- back-end/routes/userRouter.js
+++ back-end/routes/userRouter.js
@@ -3,7 +3,6 @@ import auth from "../controller/authController.js";
 const userRouter = express.Router();
 
 userRouter.post("/register",auth.registerUser );
-
 userRouter.post("/login", auth.login );
 
 export default userRouter;
